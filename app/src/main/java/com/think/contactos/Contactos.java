3a4ac55de6be2347e9ea9c0279d560de43887dd5
Julio Cesar <julio_romero1426@hotmail.com>
2016-08-04 12:18:33 -0500

    Corregido proyecto, ya utiliza la version correcta de build tools. Agregadas imagenes ligeras de prueba.

--- app/src/main/java/com/think/contactos/Contactos.java
+++ app/src/main/java/com/think/contactos/Contactos.java
@@ -26,15 +26,9 @@ public class Contactos extends AppCompatActivity {
     }
     private void inicializarContactos() {
         contactos = new ArrayList<>();
-        contactos.add(new Contacto("Jayden ","5551234233","jayden@james.com",R.drawable.aziani_jayden_jaymes_0008));
-        contactos.add(new Contacto("Jayden Jaymes","5512416345","jayden@james.com",R.drawable.aziani_jayden_jaymes_0236));
-        contactos.add(new Contacto("Jayden michele jaymes","1252353345","jayden@james.com",R.drawable.aziani_jayden_jaymes_0532));
-        contactos.add(new Contacto("Choa","5512345678","jayden@james.com",R.drawable.aziani_jayden_jaymes_0640));
-        contactos.add(new Contacto("Andrea La rau","5551234657","jayden@james.com",R.drawable.img_3733));
-        contactos.add(new Contacto("Samantha Saint","5551234895","jayden@james.com",R.drawable.img_3913));
-        contactos.add(new Contacto("Vanessa","5551234845","jayden@james.com",R.drawable.img_4161));
-        contactos.add(new Contacto("Georgina","5551234435","jayden@james.com",R.drawable.img_4492));
-        contactos.add(new Contacto("Ann Code","5551234234","jayden@james.com",R.drawable.img_4729));
-        contactos.add(new Contacto("Arlette Solis","5551234346","jayden@james.com",R.drawable.jjstock));
+        contactos.add(new Contacto("Jayden ","5551234233","jayden@james.com",R.drawable.jjass));
+        contactos.add(new Contacto("Jayden Jaymes","5512416345","jayden@james.com",R.drawable.jjbkni));
+        contactos.add(new Contacto("Jayden michele jaymes","1252353345","jayden@james.com",R.drawable.jjbw));
+        contactos.add(new Contacto("Choa","5512345678","jayden@james.com",R.drawable.jjredress));
     }
 }
